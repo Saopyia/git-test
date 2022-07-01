4475dfc298e7091b09ad1f5563e9b3336471a0ed
Saopyia <1497720678@qq.com>
2022-07-01 22:14:58 +0800

    first commit

--- src/main/java/com/Saopyia/GitTest.java
+++ src/main/java/com/Saopyia/GitTest.java
@@ -2,6 +2,6 @@ package com.Saopyia;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("你好，hellogit!");
+        System.out.println("你好，hello git!");
     }
 }
