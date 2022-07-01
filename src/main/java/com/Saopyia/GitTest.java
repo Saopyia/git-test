8e062169a17ae1583cec177a861b845d55f123e5
Saopyia <1497720678@qq.com>
2022-07-01 22:23:14 +0800

    third commit

--- src/main/java/com/Saopyia/GitTest.java
+++ src/main/java/com/Saopyia/GitTest.java
@@ -3,5 +3,6 @@ package com.Saopyia;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好，hello git!");
+        System.out.println("你好，hello git2!");
     }
 }
