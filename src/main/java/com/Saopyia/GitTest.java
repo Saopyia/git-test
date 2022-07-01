bfd899f81ced0f6cc7eb55b15eabe81c428d98c3
Saopyia <1497720678@qq.com>
2022-07-01 22:24:41 +0800

    fourth commit

--- src/main/java/com/Saopyia/GitTest.java
+++ src/main/java/com/Saopyia/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好，hello git!");
         System.out.println("你好，hello git2!");
+        System.out.println("你好，hello git2!");
     }
 }
